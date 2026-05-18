991448f24ccf4e22dfe8bef0a72fa75e103ee631
Santosnr6 <jesper-nyberg@hotmail.com>
2026-05-18 10:54:43 +0200

    add

--- mongodb-example/server.js
+++ mongodb-example/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import movieRouter from './routes/movies.route.js';
+import mongoose from 'mongoose';
 
 // Config
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8081;
+mongoose.connect(process.env.CONNECTION_STRING);
+const database = mongoose.connection;
+// console.log(database);
+
 
 // Middlewares
 app.use(express.json());
@@ -14,6 +19,12 @@ app.use(express.json());
 app.use('/api/movies', movieRouter);
 
 // Server
+
+database.on('error', (error) => console.log(error));
+database.once('connected', () => {
+    console.log('Database connected');
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
